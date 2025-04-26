39187d782852e220cb62c7cf67dca589ae23716f
Go-Colden <coldenegypt@gmail.com>
2025-04-26 18:18:59 +0000

    https://github.com/GO-Colden/Wep-programe.git

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-import wget
-
-url = "https://igwtch.icu/?video=7403498797"
-os.makedirs("website", exist_ok=True)
-
-wget.download(url, out="website/index.html")
-
-print("\n? تم تحميل الصفحة بالكامل!")
